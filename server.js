eaa9f2e06f82e3cca574de74c09c2a0ca87709a9
Trevor Stephens <treversion@yahoo.com>
2022-07-23 16:30:29 -0700

    added movies to server

--- server.js
+++ server.js
@@ -24,14 +24,25 @@ class Forecast {
     this.description = obj.weather.description;
   }
 }
+class Movies {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`;
+    this.popularity = movie.popularity;
+    this.released_date = movie.release_date;
+  }
+}
 async function handleMovies(request, response) {
-
-  console.log(request);
-  let movieKey = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=seattle`;
+  let search = request.query.query;
+  let movieKey = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${search}`;
   await axios.get(movieKey)
     .then(res => {
-      console.log(res.data.results);
-      response.send(res.data.results);
+      console.log('this is supposed to be the query:', request.query.search);
+      console.log(res.data.results.map(movie => new Movies(movie)));
+      response.send(res.data.results.map(movie => new Movies(movie)));
     })
     .catch((e) => {
       console.log(e);
